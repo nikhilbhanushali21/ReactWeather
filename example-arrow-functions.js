71d126e5f6be76a5b2d17051781a7b46e7ea1fc6
Nikhil Bhanushali <nikhilbhanushali21@gmail.com>
2017-02-08 17:33:16 -0800

    eyerything working even the  arrow function throug 05 13

--- example-arrow-functions.js
+++ example-arrow-functions.js
@@ -30,13 +30,13 @@ function add(a,b){
     return a+b;
 }
 //addStatement
-car addStatement = (a,b) => {
-    return a+b;
+var addStatement = (a,b) => {
+    return a + b;
 }
-console.log(addStatement(4,7));
+console.log(addStatement(4 , 7));
 //addExpression
-var addExpression = (a,b) => a+b;
-console.log(addExpression(9,3));
+var addExpression = (a , b) => a + b;
+console.log(addExpression(3 , -2));
 
-console.log(add(1, 3));
-console.log(add(9, 0));
+// console.log(add(1, 3));
+// console.log(add(9, 0));
