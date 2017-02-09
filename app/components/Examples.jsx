acec100176bf7d4835c5603fd496ae7fde88dfc8
Nikhil Bhanushali <nikhilbhanushali21@gmail.com>
2017-02-08 17:55:46 -0800

    through 05 14

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,17 @@
 var React = require('react');
 
-var Examples = React.createClass({
-    render: function(){
-        return (
+// var Examples = React.createClass({
+//     render: function(){
+//         return (
+//             <h3>Stuff and Things</h3>
+//         );
+//     }
+// });
+
+var Examples = (props) => {
+     return (
             <h3>Stuff and Things</h3>
-        );
-    }
-});
+        )
+};
 
 module.exports = Examples;
