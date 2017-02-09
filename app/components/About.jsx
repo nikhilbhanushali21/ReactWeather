2d022d14fbdb034be51b24ad4ff0e4ed95371011
Nikhil Bhanushali <nikhilbhanushali21@gmail.com>
2017-02-08 20:21:47 -0800

    fix tag error in about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,8 +10,10 @@ var React = require('react');
 
 var About = (props) => {
     return (
+        <div>
         <h3> About Page </h3>
         <p> Description for weather app made with reactJS</p>
+        </div>
     )
 };
 
